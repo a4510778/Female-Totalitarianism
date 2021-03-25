a890367528c4fa457046f1fd1283603d3d8f1c30
a4510778 <191429160@qq.com>
2021-03-25 20:05:51 +0800

    Modify Reason: Update Scripts
    ---
    * Update Format

--- Scripts/UpdateIdList.py
+++ Scripts/UpdateIdList.py
@@ -9,27 +9,15 @@ import os
 g_pictureDirPath = "../FemalTotalitarianismPicture"
 g_quotesDirPath = "../FemalTotalitarianismQuotes"
 
-def GetIdFromFile(fileName):
-    if len(fileName) < 0:
-        return None
-    pos = fileName.find(':') + 1
-    return fileName[pos:]
-
 def UpdateIdList():
     idList = []
     for (root, dirs, files) in os.walk(g_pictureDirPath):
         for file in files:
-            temp = GetIdFromFile(file)
-            if temp is None or len(temp) < 0:
-                continue
-            idList.append(temp)
+            idList.append(file)
 
     for (root, dirs, files) in os.walk(g_quotesDirPath):
         for file in files:
-            temp = GetIdFromFile(file)
-            if temp is None or len(temp) < 0:
-                continue
-            idList.append(temp)
+            idList.append(file)
 
     with open("../Document/IdList.txt", 'a', encoding = "UTF-8") as idListFile:
         for id in idList:
